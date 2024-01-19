c12ecc972c601c1358483a41987e5afe7be4b24b
rodybothe <rody.bothe@dyflexis.com>
2024-01-19 10:58:06 +0100

    update

--- main.tf
+++ main.tf
@@ -1,41 +1,11 @@
 provider "aws" {
     region = "eu-central-1"
-    access_key = "AKIA4BBGN7CTS4RT3UJG"
-    secret_key = "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"
 }
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
     tags = {
         Name: "${var.env_prefix}-vpc"
-    }
-}
-
-resource "aws_subnet" "myapp-subnet-1" {
-    vpc_id = aws_vpc.myapp-vpc.id
-    cidr_block = var.subnet_cidr_block
-    availability_zone = var.avail_zone
-    tags = {
-        Name: "${var.env_prefix}-subnet-1"
-    }
-}
-
-resource "aws_internet_gateway" "myapp-internet-gateway" {
-    vpc_id = aws_vpc.myapp-vpc.id
-    tags = {
-        Name: "${var.env_prefix}-igw"
-    }
-}
-
-resource "aws_route_table" "myapp-route-table" {
-    vpc_id = aws_vpc.myapp-vpc.id
-
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.myapp-internet-gateway.id
-    }
-    tags = {
-        Name: "${var.env_prefix}-rtb"
     }
 }
 
